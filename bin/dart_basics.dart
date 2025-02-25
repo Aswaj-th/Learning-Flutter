bd5875ddba35d5e5830c3a3455345c3d7d9cd0e2
Aswajith <aswajith.3747@gmail.com>
2025-02-25 16:51:41 +0530

    first commit

---
+++ bin/dart_basics.dart
@@ -0,0 +1,44 @@
+// import 'package:dart_basics/dart_basics.dart' as dart_basics;
+// import 'dart:io';
+
+void main(List<String> arguments) {
+  // print('Hello world: ${dart_basics.calculate()}!');
+  // // var a = stdin.readLineSync();
+  // // print('Hello $a');
+  // print("Enter two numbers");
+  // // var input1 = stdin.readLineSync();
+  // // var input2 = stdin.readLineSync();
+  // // var num1 = int.parse(input1!);
+  // // var num2 = int.parse(input2!);
+  // // print(num1+num2);
+  // sumParams(3, 1);
+  // sumReq(a: 7, b:6, c: 8);
+  // Future<int> ret = sumFuture(5, 6);
+  // print(ret);
+
+  sum();
+  print("After future");
+}
+
+Future<void> sum() async {
+  sumFuture(33, 44);
+  print("in sum");
+}
+
+Future<int> sumFuture(int a, int b) async {
+  await Future.delayed(Duration(seconds: 3));
+  print("in sum future ${a + b}");
+  return a + b;
+}
+
+void sumParams(int a, int b) {
+  print(a + b);
+}
+
+void sumReq({required int a, required int b, int c = 0, int? d}) {
+  print(a + b + c);
+}
+
+// Future<int> sumFuture(int a, int b) async {
+//   return a+b;
+// }
